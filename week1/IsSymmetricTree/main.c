478b00edd4805c29b35c9a53d013d97ee702d4af
liliangcan <91586917+liliangcan@users.noreply.github.com>
2021-12-12 20:34:32 +0800

    Update main.c

--- week1/IsSymmetricTree/main.c
+++ week1/IsSymmetricTree/main.c
@@ -9,7 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution {                        //has something wrong
 public:
     void InorderTraversal(TreeNode* root, vector<int>& inordercout) {
         if (root) {
@@ -29,5 +29,28 @@ public:
             return false;
         }
         return true;
+    }
+};
+
+class Solution {                    //right
+public:
+    bool check(TreeNode *u, TreeNode *v) {
+        queue <TreeNode*> q;
+        q.push(u);
+        q.push(v);
+        while (!q.empty()) {
+            u = q.front();
+            q.pop();
+            v = q.front();
+            q.pop();
+            if (!u && !v)   continue;
+            if ((!u || !v) || (u->val != v->val)) return false;
+            q.push(u->left); q.push(v->right);
+            q.push(u->right); q.push(v->left);
+        }
+        return true;
+    }
+    bool isSymmetric(TreeNode* root) {
+        return check(root, root);
     }
 };
