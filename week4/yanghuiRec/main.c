79e271a3f3559b0d8e0ae680c9130742012d1c26
liliangcan <91586917+liliangcan@users.noreply.github.com>
2021-12-26 21:20:14 +0800

    Create main.c

---
+++ week4/yanghuiRec/main.c
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> yanghui(numRows);
+        for (int i = 0; i < numRows; ++i) {
+            yanghui[i].resize(i + 1);
+            yanghui[i][0] = yanghui[i][i] = 1;          //设置初始几行值
+            for (int j = 1; j < i; ++j) {
+                yanghui[i][j] = yanghui[i - 1][j] + yanghui[i - 1][j - 1];
+            }
+        }
+        return yanghui;
+    }
+};
