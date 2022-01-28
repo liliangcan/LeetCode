88e6490bb2bf3b8a3495b048c9fdcb1c03037f27
liliangcan <91586917+liliangcan@users.noreply.github.com>
2022-01-28 14:53:21 +0800

    Create main.c

---
+++ week9/groupAnagrams/main.c
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        for (string& str: strs) {
+            string key = str;
+            sort(key.begin(), key.end());
+            //将排序完的str跟它的异位词放到一起
+            mp[key].emplace_back(str);
+        }
+        vector<vector<string>> ans;
+        for (auto it = mp.begin(); it != mp.end(); ++it) {
+            ans.emplace_back(it->second);
+        }
+        return ans;
+    }
+};
